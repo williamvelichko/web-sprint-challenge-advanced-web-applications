252946404d291c78da9e27656b104ab55ad312a1
williamvelichko <williamvelichko2003@gmail.com>
2022-01-28 08:54:31 -0800

    finished the login uth

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import { Route } from "react-router-dom";
-import styled from 'styled-components';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import styled from "styled-components";
 
-import Header from './Header';
-import BloomHeader from './BloomHeader';
-import Login from './Login';
-
+import Header from "./Header";
+import BloomHeader from "./BloomHeader";
+import Login from "./Login";
+import View from "./View";
+import Logout from "./Logout";
+import PrivateRoute from "../utils/PrivateRoute";
 const App = () => {
+  const isLoggedIn = localStorage.getItem("token");
+
   return (
     <AppContainer>
-      <BloomHeader/>
-      <Header/>
+      <BloomHeader />
+      <Header />
       <RouteContainer>
-        <Route exact path="/">
-          <Login/>
-        </Route>          
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+
+          <PrivateRoute path="view">
+            {isLoggedIn && <View component={View} />}
+          </PrivateRoute>
+          <Route path="logout">
+            <Logout />
+          </Route>
+        </Switch>
       </RouteContainer>
     </AppContainer>
-  )
-}
+  );
+};
 
 export default App;
 
@@ -28,13 +41,12 @@ export default App;
 //3. Create a PrivateRoute for View component point to '/view.'
 //4. Create a PrivateRoute for Logout component pointing to '/logout.'
 
-
 const AppContainer = styled.div`
   height: 100%;
-`
+`;
 const RouteContainer = styled.div`
   display: flex;
   height: 85%;
   align-items: center;
   flex-direction: column;
-`
+`;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,68 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useState } from "react";
+import styled from "styled-components";
+import axios from "axios";
+import { useHistory } from "react-router";
 
 const Login = () => {
-    
-    return(<ComponentContainer>
-        <ModalContainer>
-            <h1>Welcome to Blogger Pro</h1>
-            <h2>Please enter your account information.</h2>
-        </ModalContainer>
-    </ComponentContainer>);
-}
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: "",
+  });
+  const [error, setError] = useState("");
+
+  const { push } = useHistory();
+
+  const onChange = (e) => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:5000/api/login", credentials)
+      .then((resp) => {
+        console.log(resp);
+        localStorage.setItem("token", resp.data.token);
+        push("view");
+      })
+      .catch((err) => {
+        console.log(err);
+        setError("Invalid Username or Password");
+      });
+  };
+  const loginForm = localStorage.getItem("token");
+  return (
+    <ComponentContainer>
+      <ModalContainer>
+        <h1>Welcome to Blogger Pro</h1>
+        <h2>Please enter your account information.</h2>
+        <FormGroup onSubmit={handleSubmit}>
+          <Label>Username:</Label>
+          <Input
+            id="username"
+            type="text"
+            name="username"
+            value={credentials.username}
+            onChange={onChange}
+          />
+          <Label>Password:</Label>
+          <Input
+            id="password"
+            type="password"
+            name="password"
+            value={credentials.password}
+            onChange={onChange}
+          />
+          <Button id="submit">Login</Button>
+          {loginForm && <p id="error">{error}</p>}
+        </FormGroup>
+      </ModalContainer>
+    </ComponentContainer>
+  );
+};
 
 export default Login;
 
@@ -22,36 +75,36 @@ export default Login;
 //6. MAKE SURE TO ADD id="username", id="password", id="error" AND id="submit" TO THE APPROPRIATE DOM ELEMENTS. YOUR AUTOTESTS WILL FAIL WITHOUT THEM.
 
 const ComponentContainer = styled.div`
-    height: 70%;
-    justify-content: center;
-    align-items: center;
-    display:flex;
-`
+  height: 70%;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+`;
 
 const ModalContainer = styled.div`
-    width: 500px;
-    background: white;
-    padding: 2rem;
-    text-align: center;
-`
+  width: 500px;
+  background: white;
+  padding: 2rem;
+  text-align: center;
+`;
 
 const Label = styled.label`
-    display: block;
-    text-align: left;
-    font-size: 1.5rem;
-`
+  display: block;
+  text-align: left;
+  font-size: 1.5rem;
+`;
 
 const FormGroup = styled.form`
-    padding:1rem;
-`
+  padding: 1rem;
+`;
 
 const Input = styled.input`
-    font-size: 1rem;
-    padding: 1rem 0;
-    width:100%;
-`
+  font-size: 1rem;
+  padding: 1rem 0;
+  width: 100%;
+`;
 
 const Button = styled.button`
-    padding:1rem;
-    width: 100%;
-`
+  padding: 1rem;
+  width: 100%;
+`;
